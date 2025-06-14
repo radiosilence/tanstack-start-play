b12cbbf0a28a73a71c210c787f4e5dc50fa335c3
James Cleveland <james.cleveland@fresha.com>
2025-06-14 11:54:55 +0100

    more nicer example

--- app/components/Character.tsx
+++ app/components/Character.tsx
@@ -1,9 +1,13 @@
-import { graffle } from "@/graffle";
-import type { QueryFunction } from "@tanstack/react-query";
 import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
-import { graphql } from "gql.tada";
+import { graphql, type TadaDocumentNode } from "gql.tada";
+import { Graffle } from "graffle";
 import type { ComponentType } from "react";
 
+export const graffle = Graffle.create().transport({
+  url: "https://rickandmortyapi.com/graphql",
+});
+
+// example doc from r&m free api
 export const CharacterDocument = graphql(`
   query Character($id: ID!) {
     character(id: $id) {
@@ -11,30 +15,53 @@ export const CharacterDocument = graphql(`
     }
   }
 `);
-const queryFn: QueryFunction<
-  {
-    character: {
-      name: string | null;
-    } | null;
-  } | null,
-  readonly [
-    typeof CharacterDocument,
-    {
-      readonly id: string;
+
+// example doc from r&m free api
+export const CharactersDocument = graphql(`
+  query Characters {
+    characters {
+      results {
+        name
+      }
+    }
+  }
+`);
+
+export const getOptions = <
+  Data extends Record<string, unknown>,
+  Vars extends Record<string, unknown>,
+>(
+  doc: TadaDocumentNode<Data, Vars>,
+  vars?: Vars,
+) => {
+  return queryOptions({
+    queryKey: [doc, vars] as const,
+    queryFn: ({ queryKey: [doc, vars] }) => {
+      /* Argument of type '[Vars]' is not assignable to parameter of type
+      'SendArguments__<IsUnknown<Vars> extends true ? Variables : Vars, GetVariablesInputKind<IsUnknown<Vars> extends true ? Variables : Vars>>'. (ts 2345) */
+      return graffle.gql(doc).send(vars);
     },
-  ],
-  never
-> = ({ queryKey: [doc, variables] }) => {
-  return graffle.gql(doc).send(variables);
+  });
 };
 
-export const getCharacterOptions = (id: string) =>
-  queryOptions({
+// Concrete example works fine!!!
+export const getCharacterOptions = (id: string) => {
+  return queryOptions({
     queryKey: [CharacterDocument, { id }] as const,
-    queryFn,
+    queryFn: ({ queryKey: [doc, vars] }) => graffle.gql(doc).send(vars),
   });
+};
 
 export const Character: ComponentType<{ id: string }> = ({ id }) => {
-  const { data } = useSuspenseQuery(getCharacterOptions(id));
-  return <>{data?.character?.name}</>;
+  // A nice succinct function that can be used in SSR or CSR
+  const generic = useSuspenseQuery(getOptions(CharacterDocument, { id }));
+  const list = useSuspenseQuery(getOptions(CharactersDocument));
+  const concrete = useSuspenseQuery(getCharacterOptions(id));
+  return (
+    <>
+      {generic.data?.character?.name}
+      {concrete.data?.character?.name}
+      {JSON.stringify(list.data?.characters?.results)}
+    </>
+  );
 };
